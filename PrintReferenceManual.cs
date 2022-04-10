73dcb57e980ef7060c78a6501c5292f4b5ea098d
Anitha-Bhat <anithabhat97@gmail.com>
2022-04-11 00:30:05 +0530

    Add files via upload

---
+++ PrintReferenceManual.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TelCo.ColorCoder
+{
+  internal class PrintReferenceManual
+  {
+    internal static void ShowReferenceManual()
+    {
+
+      Console.WriteLine("*******************Reference Manual********************");
+      Console.WriteLine("\n");
+
+      int totalPair = ColorValues.colorMapMajor.Length * ColorValues.colorMapMinor.Length;
+
+      for (int i = 0; i < totalPair; i++)
+      {
+        ColorPair pair = EvenCountColorCode.GetColorFromPairNumber(i+1);
+
+        Console.WriteLine(
+          String.Format("Pair Number - {0},Major Color - {1}, Minor Color - {2}", i+1, pair.majorColor.Name, pair.minorColor.Name)
+        );
+        Console.WriteLine("\n");
+
+      }
+
+    }
+
+  }
+}
+
