132c5aebd9a80f78dbe6c4c1cbaebbbc2ca2a4fe
Anitha-Bhat <anithabhat97@gmail.com>
2022-04-11 00:10:07 +0530

    Add files via upload

---
+++ ColorValues.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TelCo.ColorCoder
+{
+  internal class ColorValues
+  {
+    /// <summary>
+    /// Array of Major colors
+    /// </summary>
+    internal static Color[] colorMapMajor= new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet
+    };
+    /// <summary>
+    /// Array of minor colors
+    /// </summary>
+    internal static Color[] colorMapMinor= new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+
+  }
+}
---
+++ GetPairNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TelCo.ColorCoder
+{
+ partial class EvenCountColorCode
+  {
+    /// <summary>
+    /// Given the two colors the function returns the pair number corresponding to them
+    /// </summary>
+    /// <param name="pair">Color pair with major and minor color</param>
+    /// <returns></returns>
+    internal static int GetPairNumberFromColor(ColorPair pair)
+    {
+      // Find the major color in the array and get the index
+      int majorIndex = -1;
+      for (int i = 0; i < ColorValues.colorMapMajor.Length; i++)
+      {
+        if (ColorValues.colorMapMajor[i] == pair.majorColor)
+        {
+          majorIndex = i;
+          break;
+        }
+      }
+
+      // Find the minor color in the array and get the index
+      int minorIndex = -1;
+      for (int i = 0; i < ColorValues.colorMapMinor.Length; i++)
+      {
+        if (ColorValues.colorMapMinor[i] == pair.minorColor)
+        {
+          minorIndex = i;
+          break;
+        }
+      }
+      // If colors can not be found throw an exception
+      if (majorIndex == -1 || minorIndex == -1)
+      {
+        throw new ArgumentException(
+          string.Format("Unknown Colors: {0}", pair.ToString()));
+      }
+
+      // Compute pair number and Return  
+      // (Note: +1 in compute is because pair number is 1 based, not zero)
+      return (majorIndex * ColorValues.colorMapMinor.Length) + (minorIndex + 1);
+    }
+  }
+}
